2e28ec80cc370d5982d874b5eea8dfed9bf7209a
SMakaiTakori <makait87@icloud.com>
2020-04-02 19:23:10 -0400

    created signup form, added helper methods to app controller, created routes for signup form and action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
 
+    get '/users' do
+        if logged_in?(session)
+            @users = User.all
+        else
+            redirect to '/'
+        end
+            erb :'users/index'
+    end
+    
+    get '/signup' do
+        if logged_in?(session)  
+            user = current_user(session)
+            redirect to "/users/#{user.id}"
+        end         
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        user = User.create(params)
+        if user.valid?
+            session[:user_id] = user.id        
+            redirect to "/users/#{user.id}"
+        else
+            redirect to "/signup"
+        end
+    end
 
 end
